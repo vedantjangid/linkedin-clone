697cbd81bc840cb5f190d5d2ea2d8a535871c93d
Vedant jangid <vedjangid341@gmail.com>
2022-02-27 01:15:07 +0530

    commit

---
+++ src/Sidebar.js
@@ -0,0 +1,34 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1487088678257-3a541e6e3922?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Vedant Jangid</h2>
+        <h4>@vedantjangid</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>views on post</p>
+          <p className="sidebar__statNumber">2,449</p>
+        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
